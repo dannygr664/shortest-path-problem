fbe104530e60e572bb0ce5419cee6bc1e5fabf60
Daniel Greenberg <greenberg08@gmail.com>
2018-12-09 00:56:15 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shortest-path-problem',
+    version='1.0.0',
+    description='Python implementation of a solution to the shortest path problem',
+    url='https://github.com/dannygr664/shortest-path-problem',
+    author='Daniel Greenberg',
+    author_email='greenberg08@gmail.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[],
+)
